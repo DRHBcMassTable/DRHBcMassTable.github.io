2d7d83345fcadc1d22eccf4e0669663e66df1806
XiaokaiDu <63101196+XiaokaiDu@users.noreply.github.com>
2022-08-25 16:53:39 +0800

    Update search_nuclei.js

--- js/search_nuclei.js
+++ js/search_nuclei.js
@@ -10,7 +10,7 @@ var graph_format = '.png';
 var graph_title = 'Neutron mean-field potential of: ';
 
 function UpdateTime(){
-   var update_time = "2022-07-28";
+   var update_time = "2022-08-25";
    var x = document.getElementsByClassName("upti");
    for(i=0;i<x.length;i++){
       x[i].innerHTML = update_time;
